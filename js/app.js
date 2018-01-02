962685c23b12ebaf4517767b8962eb09cb32e24d
SteffAhv <tefavv22@gmail.com>
2018-01-02 14:45:39 -0500

    añade segunda vista

--- js/app.js
+++ js/app.js
@@ -1,15 +1,8 @@
 // Inicializando el document
-$(document).ready(function(){
-	// setTimeout(function(){
-	// 	window.location.href='views/register.html';
-	// }, 2000);
-	// alert('feliz navidad');
-	// var limite = 140;
-	// var title = $('h1');
 
-	// var paragraph = $('p');
-	//Ingresando a los elementos
-	// console.log($paragraph);
-	// console.log($paragraph[0]);
-	// console.log($paragraph.eq(0).text());
+$(document).ready(function () {
+	setTimeout(function () {
+		window.location.href = 'views/register.html';
+	}, 5000);
+
 });
